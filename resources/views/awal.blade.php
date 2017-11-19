e3beac6f3de0aa9f32b14b43c4604280fed620d5
yusufnur <yusuf.n@mail.ugm.ac.id>
2017-11-19 21:40:01 +0700

    css di assets edit

--- resources/views/awal.blade.php
+++ resources/views/awal.blade.php
@@ -4,7 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="{{ asset('assets/css/w3.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/font.css') }}">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;}
 body, html {
@@ -251,6 +252,14 @@ body, html {
 <!-- Footer -->
 <footer class="w3-center w3-black w3-padding-32 w3-opacity w3-hover-opacity-off">
   <a href="/" class="w3-button w3-light-grey">HOME</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
   <p>Copyright © 2017</p>
 </footer>
 
